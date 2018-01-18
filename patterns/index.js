c696dcd68023bb105beee35268a2f7f603c0fda0
htorres09 <lcc.h.torres09@gmail.com>
2018-01-18 09:42:03 -0600

    Pattern Dict
    Diccionario de patrones

---
+++ patterns/index.js
@@ -0,0 +1,16 @@
+const patternDict = [
+    {
+        pattern: '\b(Hola|Hi|Hey)\b',
+        intent: 'Saludo'
+    },
+    {
+        pattern: '\b(Adios|Bye|Gracias)\b',
+        intent: 'Terminar'
+    },
+    {
+        pattern: '',
+        intent: ''
+    }
+];
+
+module.exports = patternDict;
