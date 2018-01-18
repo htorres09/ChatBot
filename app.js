e316babe451dd96fa3d23e3746f4a890f5306cca
htorres09 <lcc.h.torres09@gmail.com>
2018-01-18 09:19:59 -0600

    app.js
    To everyon who fork this. I recomend install nodemon in the node cmd prompt via:  npm i -g nodemon

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const ReadLine = require('readline');
+const rl = ReadLine.createInterface({
+    input:  process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+rl.setPrompt('> ');
+rl.prompt();
+rl.on('line', reply => {
+    console.log(`Has dicho ${reply}`);
+    rl.prompt();
+});
